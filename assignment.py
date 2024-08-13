f5fe02315a642f63a91668666c8933905f479d01
Legendary-cod <pure09legend@gmail.com>
2024-08-13 18:27:41 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,54 @@
+
+
+#This code represents Larry's personal information
+print("My name is Larry Kimiti")
+print("I am 14yrs")
+print("My favorite colour is Black")
+print("I live in Thika and I would like to be a commercial aviation pilot,to travel across the world for free")
+print("Coding is fun")
+  #Variables--name,age,color and city are my variable names
+
+city = "Thika"
+
+name = "Larry"
+
+age = 14
+
+color = "black"
+
+print(f"My name is {name},I live in {city},I am {age} yrs old and my favorite colour is {color}")
+  #variables--x,y and result are my variables
+
+x=12000
+y=30000
+
+print(x+y)
+print(x-y)
+print(x/y)
+
+    #variables--currentage,timepassed and thenage are my variable names
+
+currentage=14
+timepassed=5
+thenage=(currentage+timepassed)
+
+print(f"Larry is {currentage} yrs and after {timepassed} he will become {thenage} yrs old")
+
+  #variables--quote1and name are my variable names
+
+quote1="Grandpa broski skibidi lewandoski"
+name="Larry"
+quote2="Pray and never forget to repent"
+
+print(f"{name}:{quote2} and{quote1}")
+
+   #variables--name1,name2,stament1 and statement2 are my variable names
+
+name1="larry"
+name2="Rudiger"
+statement1="hi Rudiger,wanna go play some soccer"
+statement2="hi.Sure but I won't go easy on you"
+
+print(f"{name1}:{statement1}")
+print(f"{name2}:{statement2}")
+
